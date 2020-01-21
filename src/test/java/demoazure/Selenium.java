6ccec78d65c151fac42dcd20eca165cfae085e60
Alok Shrivastava <alok.shrivastava@capita.co.uk>
2020-01-21 14:13:59 +0530

    adding testng concepts

--- src/test/java/demoazure/Selenium.java
+++ src/test/java/demoazure/Selenium.java
@@ -1,38 +1,52 @@
 package demoazure;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Selenium {
 
+	WebDriver driver;
+
+	@BeforeClass()
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+	}
+
+	@AfterClass()
+	public void tearDown() {
+		driver.quit();
+	}
+
 	@Test
 	public void test1() {
 		System.out.println("azure pipeline1");
-		Assert.assertEquals(true, true);
+		driver.get("http://www.google.com");
+		Assert.assertEquals(driver.getTitle(), "Google");
 	}
 
 	@Test
 	public void test2() {
 		System.out.println("azure pipeline2");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Google");
+		driver.findElement(By.xpath("(//input[@value='Google Search'])[2]")).click();
 
-		WebDriver driver = new ChromeDriver();
-
-		driver.get("http://www.google.com");
-		driver.quit();
 	}
 
 	@Test
 	public void test3() {
 		System.out.println("azure pipeline3");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-
-		WebDriver driver = new ChromeDriver();
-
-		driver.get("http://www.google.com");
-		driver.close();
+		Assert.assertTrue(driver.getTitle().contains("Google Search"));
 	}
 
 }
