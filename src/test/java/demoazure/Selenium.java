19d6f4f8abcaa07448670a000b78544060b2a13b
Alok Shrivastava <alok.shrivastava@capita.co.uk>
2020-01-21 13:00:40 +0530

    Adding selenium code

--- src/test/java/demoazure/Selenium.java
+++ src/test/java/demoazure/Selenium.java
@@ -1,14 +1,29 @@
 package demoazure;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Selenium {
 
-	
+	WebDriver driver;
+
 	@Test
 	public void test1() {
 		System.out.println("azure pipeline");
 		Assert.assertEquals(true, true);
 	}
+
+	@Test
+	public void test2() {
+		System.out.println("azure pipeline1");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		driver.get("http://www.google.com");
+		driver.close();
+	}
+
 }
