3dccdc0289d7e74e4577271ca3e5d02033f33f0d
Alok Shrivastava <alok.shrivastava@capita.co.uk>
2020-01-21 12:35:39 +0530

    Adding project for azuree demo

---
+++ src/test/java/demoazure/Selenium.java
@@ -0,0 +1,14 @@
+package demoazure;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Selenium {
+
+	
+	@Test
+	public void test1() {
+		System.out.println("azure pipeline");
+		Assert.assertEquals(true, true);
+	}
+}
